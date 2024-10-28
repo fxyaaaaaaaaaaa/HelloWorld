92a1abc30a3d8f47dbbb7de47428c257038a024d
fxy <1545984927@qq.com>
2024-10-28 19:36:18 +0800

    first

---
+++ a.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>                                                              
+#include<unistd.h>
+#include<pthread.h>
+#include<fcntl.h>
+
+int main(int argc, char* argv[])
+{
+	printf("hello");
+    
+    return 0;
+}
+
+
